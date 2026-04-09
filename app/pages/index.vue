93d20ad37e07064a77455071b50e46e59418849d
shehab267 <shehabelhawary267@gmail.com>
2026-04-09 11:44:23 +0200

    feat: replace NuxtWelcome with NuxtPage and add homepage content

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// TODO: Replace with actual homepage content (Sprint 8)
+</script>
+
+<template>
+  <div>
+    <h1>Doworkss</h1>
+    <p>Homepage coming soon.</p>
+  </div>
+</template>
